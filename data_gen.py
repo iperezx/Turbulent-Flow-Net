7b376f6ffde3634d2398ab1afb034d0a916c95bd
iperezx <i3perez@sdsc.edu>
2023-01-23 15:07:25 -0800

    Add argparse to change the number of files to generate from the input datafile.

--- data_gen.py
+++ data_gen.py
@@ -1,7 +1,32 @@
 import numpy as np
+import torch
+import argparse
+
+parser = argparse.ArgumentParser(description='Data Generator')
+
+parser.add_argument('-i',
+                        '--input-data-file',
+                        metavar='input_data_file',
+                        type=str,
+                        default='rbc_data.pt',
+                        help='Input Data File'
+                    )
+
+parser.add_argument('-s',
+                        '--samples',
+                        metavar='samples',
+                        type=int,
+                        default=1510-100,
+                        help='Number of samples to generate train/test/val datasets.'
+                    )
+
+args = parser.parse_args()
+
+input_data_file=args.input_data_file
+samples=args.samples
 
 # read data
-data = torch.load("rbc_sample.pt")
+data = torch.load(input_data_file)
 
 # standardization
 std = torch.std(data)
@@ -15,6 +40,6 @@ data_prep = torch.stack([data[:,:,:,k*64:(k+1)*64] for k in range(7)])
 
 # use sliding windows to generate 9870 samples
 # training 6000, validation 2000, test 1870
-for j in range(0, 1510-100):
+for j in range(0, samples):
     for i in range(7):
-        torch.save(torch.FloatTensor(data_prep[i, j : j + 100]), ".../sample_" + str(j*7+i) + ".pt")
+        torch.save(torch.FloatTensor(data_prep[i, j : j + 100]), "sample_" + str(j*7+i) + ".pt")
